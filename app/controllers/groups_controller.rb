a586b4d0dc5d89b645ba46bb10b648b25ea7d276
sunadoi <y.sunadoi@gmail.com>
2019-11-25 17:25:47 +0900

    Create controller action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,26 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+
+  def new
+  end
+
+  def create
+    Group.new(group_params)
+  end
+
+  def edit
+  end
+
+  def update
+    @group.update(group_params)
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, :user_name).merge(user_id: current_user.id)
+  end
+
+  def set_group
+    @group = Group.find(params[:id]) 
+  end
 end
