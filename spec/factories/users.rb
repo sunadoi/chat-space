c1a718f16f843db6ec3596163a86bcd5d489f20b
sunadoi <y.sunadoi@gmail.com>
2019-11-26 14:49:36 +0900

    Add test code for models

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,9 @@
-factory :user do
-  password = Faker::Internet.password(min_length: 8)
-  name {Faker::Name.last_name}
-  email {Faker::Internet.free_email}
-  password {password}
-  password_confirmation {password}
+FactoryBot.define do
+  factory :user do
+    password = Faker::Internet.password(min_length: 8)
+    name {Faker::Name.last_name}
+    email {Faker::Internet.free_email}
+    password {password}
+    password_confirmation {password}
+  end
 end
