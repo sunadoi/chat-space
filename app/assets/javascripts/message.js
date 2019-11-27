b528dbf638544860bbaaf23ed4db35289ef12eeb
sunadoi <y.sunadoi@gmail.com>
2019-11-28 08:22:14 +0900

    Create ajax function on message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,15 +1,43 @@
 $(function(){
   function buildHTML(message){
-    // 「もしメッセージに画像が含まれていたら」という条件式
-    if message.image {
-      var html = //メッセージに画像が含まれる場合のHTMLを作る
+    if (message.image.url) {
+      var html = `<div class="main_chat__contents__content">
+                    <div class="main_chat__contents__content__user">
+                      <div class="main_chat__contents__content__user__name">
+                        ${message.user_name}
+                      </div>
+                      <div class="main_chat__contents__content__user__time">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="main_chat__contents__content__text">
+                      <p class="lower-message__content">
+                        ${message.content}
+                      </p>
+                      <img class="lower-message__image" src="${message.image.url}" alt="Test image">
+                    </div>
+                  </div>`
     } else {
-      var html = //メッセージに画像が含まれない場合のHTMLを作る
+      var html = `<div class="main_chat__contents__content">
+                    <div class="main_chat__contents__content__user">
+                      <div class="main_chat__contents__content__user__name">
+                        ${message.user_name}
+                      </div>
+                      <div class="main_chat__contents__content__user__time">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="main_chat__contents__content__text">
+                      <p class="lower-message__content">
+                        ${message.content}
+                      </p>
+                    </div>
+                  </div>`
     }
     return html
   }
 
-  $(".main_chat__form__submit").on("click", function(e){
+  $(".new_message").on("submit", function(e){
     e.preventDefault()
     var formData = new FormData(this);
     var url = $(this).attr('action');
@@ -21,5 +49,16 @@ $(function(){
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      var html = buildHTML(data);
+      console.log(data)
+      $('.main_chat__contents').append(html);
+      $('.main_chat__contents').animate({ scrollTop: $('.main_chat__contents')[0].scrollHeight});
+      $('.new_message')[0].reset();
+      $('.main_chat__form__submit').prop('disabled', false);
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    });
   })
 });
